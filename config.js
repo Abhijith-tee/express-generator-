4edb850f989649d0b4a3e1ff0663b09cd793bd5a
Abhijith-tee <63539211+Abhijith-tee@users.noreply.github.com>
2020-06-09 16:47:08 +0530

    passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017'
+    'mongoUrl': 'mongodb://localhost:27017',
+    'facebook': {
+        clientId:  '696155917847342',
+        clientSecret: '3685a7ef3d12c7adfa3db54a464b1d0d'
+    }
 }
